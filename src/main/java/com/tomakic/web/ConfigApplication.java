e9ebb918bd4010e97747337ad00e936a7f2e8262
Tilen Tomakić <tilen@tomakic.com>
2018-01-13 10:43:04 +0100

    Fix pom.

---
+++ src/main/java/com/tomakic/web/ConfigApplication.java
@@ -0,0 +1,9 @@
+package com.tomakic.web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/v1")
+public class ConfigApplication extends Application {
+}
---
+++ src/main/java/com/tomakic/web/ConfigProperties.java
@@ -0,0 +1,42 @@
+package com.tomakic.web;
+
+import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+import com.kumuluz.ee.configuration.cdi.ConfigValue;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@ConfigBundle("rest-config")
+public class ConfigProperties {
+
+    @ConfigValue(watch = true)
+    private String stringProperty;
+
+    private Boolean booleanProperty;
+
+    private Integer integerProperty;
+
+    public String getStringProperty() {
+        return stringProperty;
+    }
+
+    public void setStringProperty(String stringProperty) {
+        this.stringProperty = stringProperty;
+    }
+
+    public Boolean getBooleanProperty() {
+        return booleanProperty;
+    }
+
+    public void setBooleanProperty(Boolean booleanPropertysomeBoolean) {
+        this.booleanProperty = booleanPropertysomeBoolean;
+    }
+
+    public Integer getIntegerProperty() {
+        return integerProperty;
+    }
+
+    public void setIntegerProperty(Integer integerProperty) {
+        this.integerProperty = integerProperty;
+    }
+}
